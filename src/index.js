46f7c64b57c718413a42223a5b365a2960d5523c
Rohitashsharma <Rohitashsharma@gmail.com>
2018-10-04 15:10:50 +0530

    clear code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,3 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
-    // <img src="Funny-baby-boy-specs-wear-image.jpg" alt="boy pic " />
-    <img className="center" src={require('./Funny-baby-boy-specs-wear-image.jpg')} />,
-    document.getElementById('root')
-  );
